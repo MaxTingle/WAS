2929fcd00b9fd2c04f99179ee9bb714e21e8762a
Max Tingle <max@truemanswindows.co.uk>
2020-06-12 10:04:15 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WorkingAttenuationSoftware
+{
+    public partial class App : System.Windows.Application
+    {
+        private SettingWindow settingWindow = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            #region Initialize settings tray icon
+            NotifyIcon trayIcon = new NotifyIcon();
+            trayIcon.Text = "WAS";
+            trayIcon.Visible = true;
+            trayIcon.Icon = WorkingAttenuationSoftware.Properties.Resources.TrayIcon;
+            trayIcon.Click += this._TrayIconClicked;
+            #endregion
+
+            #region Initialize service
+            //TODO: Create & start service/loop/whatever to listen for mic/app sound
+            #endregion
+        }
+
+        private void _TrayIconClicked(Object sender, EventArgs e)
+        {
+            if(this.settingWindow == null)
+            {
+                this.settingWindow = new SettingWindow();
+            }
+
+            this.settingWindow.Show();
+        }
+    }
+}
---
+++ SettingWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace WorkingAttenuationSoftware
+{
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
