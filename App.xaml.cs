86cac42030bf4d6a776d48d992275155606e9e51
Max Tingle <maxting@hotmail.co.uk>
2020-08-05 16:12:09 +0100

    Added basic structure
    Pending changes once pinvoking is figured out

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,39 +1,47 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows;
 using System.Windows.Forms;
+using WorkingAttenuationSoftware.Controls;
 
 namespace WorkingAttenuationSoftware
 {
     public partial class App : System.Windows.Application
     {
-        private SettingWindow settingWindow = null;
+        private SettingWindow _SettingWindow = null;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
             #region Initialize settings tray icon
-            NotifyIcon trayIcon = new NotifyIcon();
-            trayIcon.Text = "WAS";
-            trayIcon.Visible = true;
-            trayIcon.Icon = WorkingAttenuationSoftware.Properties.Resources.TrayIcon;
+            NotifyIcon trayIcon = new NotifyIcon
+            {
+                Text = "WAS",
+                Visible = true,
+                Icon = WorkingAttenuationSoftware.Properties.Resources.TrayIcon
+            };
             trayIcon.Click += this._TrayIconClicked;
             #endregion
 
             #region Initialize service
+            AudioReducer reducer = new AudioReducer(10.0f);
+            reducer.StoreCurrentVolumeLevels();
             //TODO: Create & start service/loop/whatever to listen for mic/app sound
             #endregion
         }
 
         private void _TrayIconClicked(Object sender, EventArgs e)
         {
-            if(this.settingWindow == null)
+            if(this._SettingWindow == null)
             {
-                this.settingWindow = new SettingWindow();
+                this._SettingWindow = new SettingWindow();
+                this._SettingWindow.Closed += (closeSender, closeE) =>
+                {
+                    this._SettingWindow = null;
+                };
             }
 
-            this.settingWindow.Show();
+            this._SettingWindow.Show();
         }
     }
 }
---
+++ Controls/AudioReducer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkingAttenuationSoftware.Controls
+{
+    public sealed class AudioReducer
+    {
+        private float _Reduction;
+        private List<string> _AppWhitelist = new List<string>();
+        private List<AppVolume> _AppVolumes = new List<AppVolume>();
+
+        public AudioReducer(float reduction)
+        {
+            this._Reduction = reduction;
+        }
+
+        public void ReduceDevices()
+        {
+            this.StoreCurrentVolumeLevels();
+        }
+
+        public void SetReduction(float reduction)
+        {
+            this._Reduction = reduction;
+        }
+
+        public void StoreCurrentVolumeLevels()
+        {
+            this._AppVolumes = AudioMixer.GetAppVolumes();
+        }
+
+        public void StoreNewVolumeLevels()
+        {
+            foreach(AppVolume appVolume in AudioMixer.GetAppVolumes())
+            {
+                if(!this._AppVolumes.Any(existingAppVolume => existingAppVolume.ProcessID == appVolume.ProcessID && existingAppVolume.Path == appVolume.Path)) {
+                    this._AppVolumes.Add(appVolume);
+                }
+            }
+        }
+
+        public void ResetAppWhitelist()
+        {
+            this._AppWhitelist.Clear();
+        }
+
+        public void WhitelistApp(string path)
+        {
+            this._AppWhitelist.Add(path);
+        }
+    }
+}
